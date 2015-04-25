cbe73606c7744c99679562648a647719b3217118
Fredrick Abayie <fredrick.abayie@gmail.com>
2015-04-25 21:51:01 +0000

    Display users contact control
    Created the function to display the list of contacts
    of a particular user. This function serves as a controller for the
    contact page and the model.php

--- controllers/controller.php
+++ controllers/controller.php
@@ -17,7 +17,11 @@ if ( isset ( $_REQUEST [ 'cmd' ] ) )
      switch ( $cmd )
      {
          case 1:
-             user_login ( );
+             user_login_control ( );
+             break;
+         
+         case 2:
+             display_users_contact_control ( );
              break;
          
          default :
@@ -30,11 +34,11 @@ if ( isset ( $_REQUEST [ 'cmd' ] ) )
 /*
  * Function to 
  */
-function user_login ( )
+function user_login_control ( )
 {
     if ( isset ( $_REQUEST['username'] ) && isset ( $_REQUEST['password'] ) )
     {
-        include_once '../models.users.php';
+        include_once '../models/users.php';
         $obj = new USERS ( );
         $username = $_REQUEST['username'];
         $password = $_REQUEST['password'];
@@ -51,3 +55,36 @@ function user_login ( )
         }
     }
 }//end of user_login()
+
+
+/*
+ * Function to control the user contact display
+ * and the contacts home
+ */
+function display_users_contact_control ( )
+{
+    if ( isset ( $_SESSION ) )
+    {
+        include_once '../models/users.php';
+        $obj = new USERS ( );
+        $user_id = $_SESSION['user_id'];
+        if ( $obj->display_users_contacts ( $user_id ) )
+        {
+           $row = $obj->fetch ( );
+           echo '{"result":1,"contacts": [';
+           while ( $row )
+           {
+               json_encode($row);
+               if ( $row = $obj->fetch ( ) )
+               {
+                   echo ',';
+               }
+           }
+           echo ']}';
+        }
+        else
+        {
+             echo '{"result":0,"message":"Failed to display contacts"}';
+        }
+    }
+}//end of display_users_contact()
