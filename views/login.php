873048dc698a37d9598ab3a0188e94778d496eb7
Fredrick Abayie <fredrick.abayie@gmail.com>
2015-04-25 20:09:54 +0000

    Deleted headers from the login

--- views/login.php
+++ views/login.php
@@ -6,13 +6,6 @@
 
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-<<<<<<< HEAD
-
-
-
-=======
-      
->>>>>>> origin/michael-branch
     </head>
 
     <body class="teal darken-1">
