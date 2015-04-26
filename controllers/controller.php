61d3257eb8e4d8e1090891a67af9626e72c0cba0
kennethmensah <kenneth.mensah@ashesi.edu.gh>
2015-04-26 23:12:44 +0000

    Merge branch 'fredrickBranch' into kennethBranch
    Conflicts:
    	controllers/controller.php
    	models/adb.php
    	models/users.php
    	views/login.php

--- controllers/controller.php
+++ controllers/controller.php
@@ -23,6 +23,17 @@ if ( isset ( $_REQUEST [ 'cmd' ] ) )
          case 2:
              user_contacts_control ( );          //Call to the user_contacts_control function
 
+             break;
+
+         case 3:
+             edit_user_control ( );
+             break;
+         
+         case 4:
+             search_user_control ( );
+             break;
+
+
          default :
              echo '{"result":0, "message": "Invalid Command Entered"}';
              break;
@@ -31,7 +42,11 @@ if ( isset ( $_REQUEST [ 'cmd' ] ) )
 
 
 /*
+<<<<<<< HEAD
  * Function to 
+=======
+ * Function to create an instance of the users class
+>>>>>>> fredrickBranch
  */
 function get_user_model( )
 {
@@ -78,12 +93,17 @@ function user_login_control ( )
  */
 function user_contacts_control ( )
 {
-    if ( isset ( $_SESSION ) )
+  session_start();
+    if ( isset ( $_SESSION['user_id'] ) )
     {
-        $obj = get_user_model ( );
+        include_once '../models/contacts.php';
+        $obj2 = new CONTACTS ( );
+        $obj1 = get_user_model ( );
+//        $user_id = filter_input (INPUT_GET, 'id');
         $user_id = $_SESSION['user_id'];        //Retrieving the value in the session and assigning it to a new variable
-        if ( $obj->display_users_contacts ( $user_id ) ){
-           $row = $obj->fetch ( );      //Fetching the result
+        if ( $obj2->display_users_contacts ( $user_id ) )
+          {
+           $row = $obj2->fetch ( );       //Fetching the result
            echo '{"result":1,"contacts": [';
            while ( $row )
            {
@@ -100,4 +120,52 @@ function user_contacts_control ( )
              echo '{"result":0,"message":"Failed to display contacts"}';        //Showing an error message in json format
         }
     }
+
+
+    else
+    {
+        echo '{"result":0,"message":"Session for user id is not set"}';
+    }
 }//end of display_users_contact()
+
+
+/*
+ * Function to control the editing of a users profile
+ */
+function edit_user_control ( )
+{
+    session_start();
+    if ( isset ( $_SESSION['user_id'] ) && filter_input (INPUT_GET, 'uername') &&
+         filter_input (INPUT_GET, 'password') &&filter_input (INPUT_GET, 'profile_pic'))
+    {
+        $obj = get_user_model( );
+        $user_id = $_SESSION['user_id'];
+        $username = filter_input ( INPUT_GET, 'username' );
+        $password = filter_input ( INPUT_GET, 'password' );
+        $profile_pic = filter_input ( INPUT_GET, 'profile_pic' );
+
+        if ( $obj->edit_user ( $user_id, $username, $password, $profile_pic ) )
+        { 
+            echo '{"result":1,"message":"User profile successfully updated"}';
+        }
+        else
+        {
+            echo '{"result":0,"message":"Failed to update user profile"}';
+        }
+    }
+    else
+    {
+        echo '{"result":0,"message":"All fields are not set"}';
+    }
+}//end of edit_user_control()
+
+
+/*
+ * Function to control the searching of a new user
+ */
+function search_user_control ( )
+{
+    session_start();
+    $obj = get_user_model( );
+}//search_user_control()
+
