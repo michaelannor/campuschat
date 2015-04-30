47f6ceaad5f4b78b758f079b473227021c859a57
kennethmensah <kenneth.mensah@ashesi.edu.gh>
2015-04-30 22:28:53 +0000

    added delete contact

--- models/contacts.php
+++ models/contacts.php
@@ -51,7 +51,7 @@ Class CONTACTS extends adb
     /*
      * Function to add contacts
      */
-    function add_contacts($user_sender, $user_receiver)
+    function add_contact($user_sender, $user_receiver)
     {
         $str_sql = "INSERT INTO campuschat_contacts SET
                     user_sender = $user_sender,
@@ -60,5 +60,17 @@ Class CONTACTS extends adb
         return $this->query($str_sql);
     }
     
+    
+    /*
+     * Function to delete contact
+     */
+    function delete_contact($user_sender, $user_receiver)
+    {
+        $str_sql = "DELETE FROM campuschat_contacts
+                where user_sender = $user_sender and
+                user_receiver = $user_receiver";
+        
+        return $this->query($str_sql);
+    }
 }
 
